f230343f580ad66a0713ded6cc0cbcf0f66cafd7
Jhony Palmer <jhonypalmer13@gmail.com>
2018-07-17 17:47:04 -0300

    Add file scheme ex-4

---
+++ Trabalho/scheme ex-4.scm
@@ -0,0 +1,28 @@
+(define (dia-semana dia mes ano)
+  (cond
+    ((= mes 1) (set! mes 11) (set! ano(- ano 1)))
+    ((= mes 2) (set! mes 12) (set! ano(- ano 1)))
+    ((= mes 3) (set! mes 1))
+    ((= mes 4) (set! mes 2))
+    ((= mes 5) (set! mes 3))
+    ((= mes 6) (set! mes 4))
+    ((= mes 7) (set! mes 5))
+    ((= mes 8) (set! mes 6))
+    ((= mes 9) (set! mes 7))
+    ((= mes 10) (set! mes 8))
+    ((= mes 11) (set! mes 9))
+    ((= mes 12) (set! mes 10))
+  )
+
+(define seculo (div ano 100))
+(define mint (div (- (* 13 mes) 1) 5))
+(define aint (div ano 4))
+(define sint(div seculo 4))
+(define x (- (+ mint aint sint ano dia)(* 2 seculo)))
+(define diaSemana (mod x 7))
+
+(cond
+  ((= diaSemana -1) (set! diaSemana 6))
+)
+(print "o dia da semana é: " diaSemana)
+)
